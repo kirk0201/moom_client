3fef70c45e40f0c38f85141dce19529a7acb51e7
wlwl0219 <wlwl0219@gmail.com>
2020-12-05 17:00:19 +0900

    IMPROVE github button

--- moom/src/components/HLogin.js
+++ moom/src/components/HLogin.js
@@ -3,21 +3,29 @@ import GitHubLogin from "react-github-login";
 
 class HLogin extends Component {
   render() {
-    const onSuccessGithub = (response) => {
-      console.log(response.code);
-    };
+    // const onSuccessGithub = (response) => {
+    //   console.log(response.code);
+    // };
 
-    const onFailureGithub = (response) => {
-      console.log(response.code);
-    };
+    // const onFailureGithub = (response) => {
+    //   console.log(response.code);
+    // };
+
+    // const button = document.querySelector(".githib");
+    // const div = document.createElement("div");
+    // div.className = "github-div";
+    // const img = document.createElement("img");
+    // img.className = "github-img";
+    // img.src = profile_img;
+    // const span = document.createElement("span");
+    // span.className = "github-span";
+    // span.textContent = "Github";
 
     return (
       <div>
         <GitHubLogin
           className="githib"
           clientId="c30e06847f78a8951b9c"
-          onSuccess={onSuccessGithub}
-          onFailure={onFailureGithub}
           buttonText="Github"
           valid={true}
         />
