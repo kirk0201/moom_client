02bf8940a0ade8c506a641ec5dc7232bf6461d23
wlwl0219 <wlwl0219@gmail.com>
2020-12-09 13:13:42 +0900

    REFACTOR components

--- moom/src/pages/SignupPage.js
+++ moom/src/pages/SignupPage.js
@@ -1,13 +1,9 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 import SignUp from "../components/SignUp";
 
 class SignupPage extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   // SignUp 회원가입 요청을 담은 컴포넌트 렌더
   render() {
     return <SignUp />;
---
+++ moom/test.js
@@ -0,0 +1,25 @@
+index
+ㄴ> App (로그인 성공/실패에 따른 axios 유저정보get setState)
+    ㄴ> LoginMain
+        ㄴ> LoginNav (axios 로그아웃 )
+        ㄴ> BasicBody
+            ㄴ> CertainBody
+        ㄴ> CustomBody
+            ㄴ> CertainBody
+        ㄴ> MyPage
+            ㄴ> UserInfo
+                ㄴ> UserInfoEdit (axios 유저정보put setState)
+                ㄴ> UserInfoImg (axios 유저정보put setState)
+                ㄴ> UserInfoSex (axios 유저정보put setState)
+                ㄴ> SignOutModal (axios 회원탈퇴 setState)
+                    ㄴ> CheckModal
+ㄴ> App (로그인 성공/실패에 따른 axios 유저정보get setState)
+    ㄴ> NoLoginMain
+        ㄴ> NoLoginNav
+        ㄴ> Introduce
+        ㄴ> Experience
+        ㄴ> LoginPage
+            ㄴ> LogIn
+                ㄴ> GLogin
+        ㄴ> SignupPage
+            ㄴ> SignUp
