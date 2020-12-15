12769e8b378c766c5c4f290311f7c2a4d79656e9
ksy <zmdkdkt321@gmail.com>
2020-12-15 11:42:47 +0900

    material nav

--- moom/src/components/NoLoginNav.js
+++ moom/src/components/NoLoginNav.js
@@ -1,44 +1,86 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../css/NologinNav.css";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import logoimg from "../images/moomlogo.png";
 
 const NoLoginNav = () => {
   // 비로그인시 네비 바
 
   //TODO: 고정 nav바 css style
-  const style = {
-    position: "fixed",
-    top: 0,
-    width: "100%",
-    height: "35px",
-    paddingTop: "20px",
-    backgroundColor: "gray",
-  };
+  const useStyles = makeStyles({
+    nav: {
+      display: "block",
+      height: "60px",
+      minHeight: "auto",
+    },
+    login: {
+      color: "white",
+    },
+    logout: {
+      color: "white",
+    },
+  });
+
+  const classes = useStyles();
 
   return (
     <div>
-      {/* TODO: TOP 최상위버튼 */}
-      <header style={{ ...style }}>
-        <Link to="/">
-          <button>로고</button>
-        </Link>
-        <Link to="/login">
-          <button>로그인</button>
-        </Link>
-        <Link to="/signup">
-          <button>회원가입</button>
-        </Link>
-        <button
-          style={{
-            cursor: "pointer",
-            position: "fixed",
-            right: 20,
-          }}
+      <header style={{ width: "100%", display: "block", minHeight: "auto" }}>
+        <AppBar>
+          <Toolbar
+            classes={{
+              root: classes.nav,
+            }}
+          >
+            <Link to="/">
+              <img
+                src={logoimg}
+                style={{
+                  width: "50px",
+                  height: "auto",
+                  marginTop: "5px",
+                }}
+              />
+            </Link>
+            <Link to="/signup" style={{ float: "right", marginTop: "12px" }}>
+              <Button
+                color="inherit"
+                classes={{
+                  root: classes.login,
+                }}
+              >
+                회원가입
+              </Button>
+            </Link>
+            <Link to="/login" style={{ float: "right", marginTop: "12px" }}>
+              <Button
+                color="inherit"
+                classes={{
+                  root: classes.logout,
+                }}
+              >
+                로그인
+              </Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </header>
+
+      <div className="topbutton">
+        <Button
+          variant="contained"
+          color="primary"
           onClick={() => window.scrollTo(0, 0)}
         >
           TOP
-        </button>
-      </header>
+        </Button>
+      </div>
     </div>
   );
 };
