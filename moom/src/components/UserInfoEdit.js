4ec7f46acba8c03fbf1b88bc23da7adacb0724a1
limdonghyun88 <ehdrhzle3@gmail.com>
2020-12-17 16:40:10 +0900

    update/96-MypageCss

--- moom/src/components/UserInfoEdit.js
+++ moom/src/components/UserInfoEdit.js
@@ -56,15 +56,56 @@ class UserInfoEdit extends Component {
     const { info, noInfo, name, type, what } = this.props;
     return (
       <>
-        <input
-          name={name} //"promise"
-          type={type}
-          placeholder={info ? info : noInfo}
-          onChange={this.handleInputUserEdit}
-        />
-        <button name={what} onClick={this.handleUserEdit}>
-          저장
-        </button>
+      <hr />
+          <div class="md:inline-flex w-full space-y-4 md:space-y-0 p-8 text-gray-500 items-center">
+            <h2 class="md:w-4/12 max-w-sm mx-auto">정보변경</h2>
+
+            <div class="md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2">
+              <div class="w-full inline-flex border-b">
+                <div class="w-1/12 pt-2">
+                <svg
+                      fill="none"
+                      class="w-6 text-gray-400 mx-auto"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                </div>
+                <input
+                          name={name} //"promise"
+                          type={type}
+                          placeholder={info ? info : noInfo}
+                          onChange={this.handleInputUserEdit}
+                  class="w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4"
+                />
+              </div>
+            </div>
+
+            <div class="md:w-3/12 text-center md:pl-6">
+              <button name={what} onClick={this.handleUserEdit} class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right">
+                <svg
+                  fill="none"
+                  class="w-4 text-white mr-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  />
+                </svg>
+                저장
+              </button>
+            </div>
+          </div>
       </>
     );
   }
