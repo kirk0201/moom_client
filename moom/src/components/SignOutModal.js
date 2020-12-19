43362b4becb52b46dbd50b50e93109729e67e866
kimjaehyen <mine6546@gmail.com>
2020-12-19 15:23:07 +0900

    Update  SignOut-modal-CSS

--- moom/src/components/SignOutModal.js
+++ moom/src/components/SignOutModal.js
@@ -4,7 +4,7 @@ import { withRouter } from "react-router-dom";
 import { BASEURL } from "../helpurl";
 import "../css/SignOutModal.css";
 import "../css/CheckModal.css";
-
+import "../index.css";
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
@@ -45,22 +45,40 @@ class SignOutModal extends Component {
 
   render() {
     const { open, close } = this.props;
+
     return (
       <>
         {open ? (
           <div className="modal">
-            <div className="signout_modal">
-              <div>홍길동님, 정말 계정을 삭제하실건가요?</div>
-              <div>moom 계정을 삭제하면</div>
-              <div>홍길동님이 생성한 모든 기록이 함께 사라집니다</div>
-
+            <div className="rounded-t-2xl rounded-b-2xl text-lg signout_modal">
+              <div className=" w-100 border-solid h-10 pt-2 pl-2 text-center bg-gray-800 rounded-t-2xl text-white">
+                회원 탈퇴
+              </div>
+              <div className="text-left ml-2 px-5 py-5">
+                <span className="font-bold animate-pulse text-2xl pr-2">
+                  {this.props.username}
+                </span>
+                님, 정말 계정을 삭제하실건가요?
+              </div>
+              <div className="text-left ml-2 px-5">
+                moom 계정을 삭제하면 생성한 모든 기록이 함께 사라집니다
+              </div>
+              {/* <div className="text-left ml-2"></div> */}
+              <div className="text-left border-solid border mt-7 border-gray-200 mx-10"></div>
               {/* 취소 버튼 */}
-              <button className="modal_close" onClick={close}>
+              <button
+                className=" mb-5 modal_close mt-5 border-solid border focus:outline-none hover:bg-gray-200 focus:bg-gray-400 border-black px-2 mr-10 rounded-md"
+                onClick={close}
+              >
                 취소
               </button>
               {/* 삭제 버튼 */}
-              <button onClick={this.checkmodal}>계정 삭제</button>
-
+              <button
+                className="border-solid border focus:outline-none border-black px-2 mr-5 focus:bg-gray-400 hover:bg-gray-200 rounded-md"
+                onClick={this.checkmodal}
+              >
+                계정 삭제
+              </button>
               {/*삭제 버튼을 클릭하여 state가 바뀌면 아래 컴퍼넌트에 있는 
               상항연산자에 의해 모달창이 render됨 */}
               <CheckModal
@@ -85,12 +103,25 @@ export function CheckModal(props) {
     <>
       {props.open ? (
         <div className="check">
-          <div className="check_modal">
-            <div>정말로 삭제하시겠습니까?</div>
-
+          <div className="check_modal bg-white rounded-2xl">
+            <div className="border-solid border border-black w-full h-10 bg-gray-800 text-center text-white rounded-t-2xl">
+              확인
+            </div>
+            <div className="py-5 text-left px-4">정말로 삭제하시겠습니까?</div>
+            <div className="border border-solid border-gray-200 mx-10"></div>
             {/* SignOutModal에서 받은 handleSignout 함수 */}
-            <button onClick={props.signout}>네</button>
-            <button onClick={props.close}>아니오</button>
+            <button
+              className="mt-4 mr-5 border-solid focus:bg-gray-400 border-black border hover:bg-gray-200 focus:outline-none mb-5 px-2 rounded-md"
+              onClick={props.signout}
+            >
+              네
+            </button>
+            <button
+              className="border border-solid focus:bg-gray-400 border-black rounded-md px-2 focus:outline-none hover:bg-gray-200 mr-4"
+              onClick={props.close}
+            >
+              아니오
+            </button>
           </div>
         </div>
       ) : null}
